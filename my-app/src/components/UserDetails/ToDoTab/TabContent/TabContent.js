96235fd1e4c13df750aa21c9a677d7ac86ad1569
Roman <roman.if.ko@gmail.com>
2023-12-13 23:51:55 -0600

    added event prevent default

--- my-app/src/components/UserDetails/ToDoTab/TabContent/TabContent.js
+++ my-app/src/components/UserDetails/ToDoTab/TabContent/TabContent.js
@@ -2,11 +2,15 @@ import React from 'react';
 
 const TabContent = ({ label, active, onClick }) => {
 
-
+    const handleClick = (event) => {
+        event.preventDefault(); 
+        onClick(event); 
+      };
+      
   return (
     <div
       className={`todotab__element ${active ? 'active' : ''}`}
-      onClick={onClick}
+      onClick={handleClick}
     >
       <h3>{label}</h3>
     </div>
